069b40843ee2c1b965d27ca7c46aace079b75bab
theakashsarkar <theakashsarkar7@gmail.com>
2025-05-06 23:34:23 +0600

    feat: improve category scraping regex pattern

--- webCrawling/crawl_website.py
+++ webCrawling/crawl_website.py
@@ -49,7 +49,15 @@ import requests
 import logging
 
 def get_category_list(content):
-    return category_pat.findall(content)
+    # print(content)
+    # category_pat = re.compile(r'<li><a href="(catalogue/category/books/.*?)">\s*(\w+[\s\w]+\w)\s*<', re.MULTILINE | re.DOTALL)
+    # result = re.findall(r'<li>\s*<a[^>]*?href=\"(catalogue/category/books/[^\"]+)\">', content,re.MULTILINE | re.DOTALL)
+    # result = re.findall(r'<li><a href="(catalogue/category/books/.*?)">\s*(\w+[\s\w]+\w)\s*<', re.MULTILINE | re.DOTALL)
+    result = re.findall(r'<li>\s*<a href=\"(catalogue/category/books_1/.*?)\">\s*(\w+[\s\w]+\w)\s*?</a>', content, re.MULTILINE | re.DOTALL)
+    print(result)
+    return result
+
+    # return category_pat.findall(content)
 
 def get_page_content(url):
     try:
@@ -65,20 +73,24 @@ def get_page_content(url):
 def crawl_category(category_name, category_url):
     logging.info(f'Crawling category: {category_name} from {category_url}')
     # Add your category crawling logic here
-    pass
+    # pass
 
 def crawl_website():
     url = "https://books.toscrape.com/index.html"
     host_name = "books.toscrape.com"
     content = get_page_content(url)
+    # print(content)
     if content is None:
         logging.critical(f"Got empty content from url: {url}")
         sys.exit(1)
     category_list = get_category_list(content)
+    # print(category_list)
     for category in category_list:
-        category_url, category_name = category 
-        category_url = "https://" + host_name + "/" + category_url
-        crawl_category(category_name, category_url)
+        print(category)
+        # category_url, category_name = category 
+        # print(category)
+        # category_url = "https://" + host_name + "/" + category_url
+        # crawl_category(category_name, category_url)
 
 if __name__ == "__main__":
     category_pat = re.compile(r'<li><a href="(catalogue/category/books/.*?)">\s*(\w+[\s\w]+\w)\s*<', re.MULTILINE | re.DOTALL)
