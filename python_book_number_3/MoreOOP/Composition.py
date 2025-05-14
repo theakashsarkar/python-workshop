06d8e133bdf19b00b22a9cc0bdb1ac6bf76b77cf
theakashsarkar <theakashsarkar7@gmail.com>
2025-05-14 16:05:42 +0600

    composition

---
+++ python_book_number_3/MoreOOP/Composition.py
@@ -0,0 +1,12 @@
+class Jhontu:
+  def __init__():
+    self.n = Nonte()
+    self.f = Fonte()
+    self.m = Monte()
+
+  def math_marks():
+    return self.n.math_marks()
+  def english_marks():
+    return self.f.english_marks()
+  def science_marks():
+    return self.m.science_marks()
