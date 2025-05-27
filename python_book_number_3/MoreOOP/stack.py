c7d2af33c94d1f8d766b8d07e86075b5e99156a2
Dev 01 <dev01@Devs-MacBook-Pro.local>
2025-05-27 13:33:14 +0600

    quequ and stack implement

--- python_book_number_3/MoreOOP/stack.py
+++ python_book_number_3/MoreOOP/stack.py
@@ -1,18 +1,34 @@
+# list = []
+
+# def push(item):
+#     if len(list) < 5:
+#        return list.append(item)
+#     print('Stack overflow')
+# def pop():
+#     print(list.pop())
+
+# push(5)
+# push(6)
+# push(7)
+# push(8)
+# push(9)
+
+# print(len(list))
+
 list = []
 
 def push(item):
     if len(list) < 5:
-       return list.append(item)
-    print('Stack overflow')
+        list.append(item)
+
 def pop():
-    print(list.pop())
+    return list.pop(0)
 
-push(5)
-push(6)
-push(7)
-push(8)
-push(9)
-pop()
-push(10)
-pop()
+push(1)
+push(2)
+push(3)
+push(4)
 
+print(pop())
+print(pop())
+print(pop())
---
+++ python_book_number_3/MoreOOP/stackOOP.py
@@ -0,0 +1,13 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        if len(self.items) < 5:
+            self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+
+
